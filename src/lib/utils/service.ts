9c7567d8a1e5bcc309ccb4ac606b5eb599e0d94f
amu <dommolino28@gmail.com>
2025-02-12 14:53:36 +0800

    Update service.ts

--- src/lib/utils/service.ts
+++ src/lib/utils/service.ts
@@ -6,7 +6,7 @@ const api = axios.create({
 });
 
 const getHeaders = (token?: string, isFormData?: boolean) => {
-	const headers: { [key: string]: string } = {};
+	const headers: Record<string, string> = {};
 
 	if (token) {
 		headers['Authorization'] = `Bearer ${token}`;
@@ -21,28 +21,24 @@ const getHeaders = (token?: string, isFormData?: boolean) => {
 
 export default async function dataFetch(
 	endpoint: string,
-	method: string,
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
 	data?: any,
 	token?: string,
 	responseType?: ResponseType
 ) {
 	try {
 		const isFormData = data instanceof FormData;
-
 		const response = await api.request({
 			url: endpoint,
 			method,
-			data,
+			data: data ? (isFormData ? data : JSON.stringify(data)) : undefined,
 			headers: getHeaders(token, isFormData),
 			responseType: responseType || 'json'
 		});
+
 		return response.data;
-	} catch (error: unknown) {
-		if (error instanceof Error) {
-			console.log(token);
-			throw new Error(error.message);
-		} else {
-			throw new Error('An unknown error occurred');
-		}
+	} catch (error) {
+		console.error('API request error:', error);
+		throw new Error(error instanceof Error ? error.message : 'An unknown error occurred');
 	}
 }
